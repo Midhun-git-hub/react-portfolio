7b442a94a6822f45b341ba0dc7150bf93eb29b5e
Midhun-git-hub <m20787549@gmail.com>
2026-03-22 10:13:10 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import About from './components/About.jsx';
@@ -20,6 +21,7 @@ const App = () => {
         <Contact />
         <Footer />
       </main>
+      <Analytics />
       </div>
     </>
   )
